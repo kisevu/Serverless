4f08194fedef50ba8c4e6b83003c2c9f7bd8f05b
kisevu <amedakevin@gmail.com>
2025-08-03 01:02:45 +0300

    spring cloud stream functionality, with clear declarative and plug and play broker setups through well declarative binders

--- src/main/java/com/ameda/kevin/news_letter/functions/Subscribers.java
+++ src/main/java/com/ameda/kevin/news_letter/functions/Subscribers.java
@@ -2,6 +2,7 @@ package com.ameda.kevin.news_letter.functions;
 
 import com.ameda.kevin.news_letter.model.Subscriber;
 import com.ameda.kevin.news_letter.service.SubscriberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ import java.util.function.Supplier;
  */
 
 @Configuration
+@Slf4j
 public class Subscribers {
 
     private final SubscriberService subscriberService;
@@ -27,8 +29,29 @@ public class Subscribers {
         return subscriberService::findAll;
     }
 
+    /*
+    *  use of Supplier in the context of spring cloud stream,
+    * then it automatically gets treated as a stream of messages ( continuous messages).
+    *  The below setup will produce a message every second for that matter.
+    *  We could introduce a poller functionality in the yaml file, and in my case i try to
+    *  produce data after 5secs
+    *
+    * */
+
+    @Bean
+    public Supplier<Subscriber>  sendSubscribersInfo(){
+        return () -> {
+            Subscriber subscriber = Subscriber
+                    .builder()
+                    .email("amedakevin@gmail.com")
+                    .build();
+            log.info(" Sending :{}" ,subscriber.getEmail());
+            return subscriber;
+        };
+    }
+
     @Bean
     public Consumer<String> create(){
-        return subscriberService::create;
+      return subscriberService::create;
     }
 }
---
+++ src/main/java/com/ameda/kevin/news_letter/streams/KafkaProducerStreams.java
@@ -0,0 +1,9 @@
+package com.ameda.kevin.news_letter.streams;
+
+/**
+ * Author: kev.Ameda
+ */
+
+
+public class KafkaProducerStreams {
+}
